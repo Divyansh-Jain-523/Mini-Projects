7e313150d980d5f627fcf603c8b3d6ebf873ff0a
Divyansh Jain <divyansh200323@gmail.com>
2024-04-02 00:41:08 +0530

    👍

--- Mini Projects/PROJECT 5: BANKIST APP/script.js
+++ Mini Projects/PROJECT 5: BANKIST APP/script.js
@@ -7,7 +7,8 @@ const divyansh = {
   lName: "Jain",
   age: 21,
   pin: 1821,
-  transactions: [212, 24, 64, 78, 5, 346, 68],
+  transactions: [1000, -200 , 100 , 1000 , -500 , 200 , -100 , -50],
+  currency: "$",
 };
 
 const shainky = {
@@ -15,29 +16,59 @@ const shainky = {
   lName: "Jain",
   pin: 1212,
   transactions: [212, 24, 64, 78, 5, 346, 68],
+  currency: "R",
 };
 
 const users = [divyansh, shainky];
+users.forEach(user => user.id = (user.fName[0] + user.lName[0]).toLowerCase());
 
-let currentUser;
+let action;
 
+
+// functions
+
+function login() {
+  for (const user of users)
+    if (
+      user.id === userInput.value &&
+      Number(pinInput.value) === user.pin
+    ) {
+      document.querySelector("#main-box").classList.remove("hidden");
+      document.querySelector("#main-left").innerHTML = null;
+      updateInfo.apply(user);
+      addTransactions.apply(user);
+      break;
+    }
+  userInput.value = null;
+  pinInput.value = null;
+}
+
+function updateInfo() {
+  document.querySelector(
+    "#login-text"
+  ).textContent = `Welcome ${this.fName}`;
+  let sum = this.transactions.reduce((a,b) => a+b);
+  document.querySelector("#bal-info").textContent = sum + this.currency;
+}
+
+function addTransactions() {
+  for(let amount of this.transactions){
+    const operation = amount > 0 ? 'deposit' : 'withdrawal';
+    const code = `
+    <div class="entry">
+      <p class="${operation}">${operation}</p>
+      <h4 class="amount">${Math.abs(amount)}</h4>
+    </div>`;
+
+    action = 'afterBegin'; // needs to change
+    document.querySelector('#main-left').insertAdjacentHTML(action,code);
+  }
+}
+
+
+// eventHandlers
 document.querySelector("#login-btn").addEventListener("click", login);
 
 pinInput.addEventListener("keydown", function (e) {
   if (e.key === "Enter") login();
 });
-
-function login() {
-  for (const user of users)
-    if (
-      user.fName[0].concat(user.lName[0]).toLowerCase() === userInput.value &&
-      Number(pinInput.value) === user.pin
-    ) {
-      currentUser = user;
-      document.querySelector("#main-box").classList.remove("hidden");
-      break;
-    }
-
-  userInput.value = "";
-  pinInput.value = "";
-}
