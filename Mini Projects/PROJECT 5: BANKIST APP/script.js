0b69b3b7df74ffef6af006f39d19f1fe8a71cf24
Divyansh Jain <divyansh200323@gmail.com>
2024-04-03 22:29:22 +0530

    mostly done

--- Mini Projects/PROJECT 5: BANKIST APP/script.js
+++ Mini Projects/PROJECT 5: BANKIST APP/script.js
@@ -1,13 +1,14 @@
 "use strict";
 const userInput = document.querySelector("#user");
 const pinInput = document.querySelector("#pin");
+let currentUser;
 
 const divyansh = {
   fName: "Divyansh",
   lName: "Jain",
   age: 21,
   pin: 1821,
-  transactions: [1000, -200 , 100 , 1000 , -500 , 200 , -100 , -50],
+  transactions: [1000, -200, 100, 1000, -500, 200, -100, -50],
   currency: "$",
 };
 
@@ -16,27 +17,35 @@ const shainky = {
   lName: "Jain",
   pin: 1212,
   transactions: [212, 24, 64, 78, 5, 346, 68],
-  currency: "R",
+  currency: "€",
 };
 
 const users = [divyansh, shainky];
-users.forEach(user => user.id = (user.fName[0] + user.lName[0]).toLowerCase());
+users.forEach(
+  (user) => (user.id = (user.fName[0] + user.lName[0]).toLowerCase())
+);
+users.forEach(
+  (user) =>
+    (user.balance = user.transactions.reduce((acc, cur) => acc + cur, 0))
+);
 
 let action;
 
-
 // functions
+
+function updateAll() {
+  updateInfo.apply(currentUser);
+  displayTransactions.apply(currentUser);
+  for (let box of document.querySelectorAll(".inp-box")) box.value = null;
+}
 
 function login() {
   for (const user of users)
-    if (
-      user.id === userInput.value &&
-      Number(pinInput.value) === user.pin
-    ) {
+    if (user.id === userInput.value && Number(pinInput.value) === user.pin) {
+      currentUser = user;
       document.querySelector("#main-box").classList.remove("hidden");
       document.querySelector("#main-left").innerHTML = null;
-      updateInfo.apply(user);
-      addTransactions.apply(user);
+      updateAll();
       break;
     }
   userInput.value = null;
@@ -44,27 +53,50 @@ function login() {
 }
 
 function updateInfo() {
-  document.querySelector(
-    "#login-text"
-  ).textContent = `Welcome ${this.fName}`;
-  let sum = this.transactions.reduce((a,b) => a+b);
-  document.querySelector("#bal-info").textContent = sum + this.currency;
+  document.querySelector("#login-text").textContent = `Welcome ${this.fName}`;
+  document.querySelector("#bal-info").textContent =
+    this.balance + this.currency;
 }
 
-function addTransactions() {
-  for(let amount of this.transactions){
-    const operation = amount > 0 ? 'deposit' : 'withdrawal';
+function displayTransactions() {
+  for (let amount of this.transactions) {
+    const operation = amount > 0 ? "deposit" : "withdrawal";
     const code = `
     <div class="entry">
       <p class="${operation}">${operation}</p>
-      <h4 class="amount">${Math.abs(amount)}</h4>
+      <h4 class="amount">${Math.abs(amount) + this.currency}</h4>
     </div>`;
 
-    action = 'afterBegin'; // needs to change
-    document.querySelector('#main-left').insertAdjacentHTML(action,code);
+    action = "afterBegin"; // needs to change
+    document.querySelector("#main-left").insertAdjacentHTML(action, code);
   }
 }
 
+function transfer() {
+  const amount = Number(document.querySelector("#transfer-amount").value);
+  const benificery = document.querySelector("#reciever").value;
+  if (currentUser.balance >= amount) {
+    for (let user of users) {
+      if (user.id === benificery) {
+        currentUser.transactions.push(-amount);
+        currentUser.balance -= amount;
+        user.balance += amount;
+        user.transactions.push(amount);
+        updateAll();
+        break;
+      }
+    }
+  }
+}
+
+function loan() {
+  const amount = Number(document.querySelector("#loan-amount").value);
+  setTimeout(() => {
+    currentUser.transactions.push(amount);
+    currentUser.balance += amount;
+    updateAll();
+  }, 5000);
+}
 
 // eventHandlers
 document.querySelector("#login-btn").addEventListener("click", login);
@@ -72,3 +104,6 @@ document.querySelector("#login-btn").addEventListener("click", login);
 pinInput.addEventListener("keydown", function (e) {
   if (e.key === "Enter") login();
 });
+
+document.querySelector("#transfer-btn").addEventListener("click", transfer);
+document.querySelector("#loan-btn").addEventListener("click", loan);
